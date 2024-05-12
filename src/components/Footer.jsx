9f85ce25ca68d0f8bf999ada8932e1329cf23a6b
Donatello Fantauzzi <donatello.fantauzzi@gmail.com>
2024-05-12 20:01:53 +0200

    Added privacy policy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className=" dark:bg-darkSecondary mx-auto flex h-full w-full items-center   justify-center bg-primary p-8 ">
+    <footer className=" mx-auto flex h-full w-full items-center justify-center   bg-primary p-8 dark:bg-darkSecondary ">
       <p className=" text-background">
         Copyright &#169; 2023 Donatello Fantauzzi. Tutti i diritti riservati
       </p>
